ad27fb84b8bf722f85520d4990eb9153a4b67517
Abhinandan Mittal <155797902+mittalabhinandan@users.noreply.github.com>
2024-02-08 11:31:34 +0530

    Folder Structure Updated

--- src/AffliatePage/AffliatePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AffliatePage = () => {
-  return (
-    <div  className='fs-3'>This is Affliate Dashboard</div>
-  )
-}
-
-export default AffliatePage 
--- src/MainUrl/MainUrl.js
+++
@@ -1,4 +0,0 @@
-// export const  Main_URL = " https://growxad-rm2l.onrender.com"
-// export const  Main_URL = "https://updated-backend.onrender.com"
-// export const  Main_URL = "http://localhost:1337"
-export const Main_URL = "https://growbackend-9b25ec9109f6.herokuapp.com";
