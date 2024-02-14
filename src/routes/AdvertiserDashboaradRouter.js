4743e665d2878c158f8e135930a2129a6fe06b1d
Abhishek <abhibadh03@gmail.com>
2024-02-14 12:27:03 +0530

    finance Route added

--- src/routes/AdvertiserDashboaradRouter.js
+++ src/routes/AdvertiserDashboaradRouter.js
@@ -3,10 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import AdvertiserDashbaordLayout from './../features/AdvertiserDashboard/components/Layout/index';
 import CreateCampaign from '../features/AdvertiserDashboard/pages/CreateCampaign';
 import MyCampaign from '../features/AdvertiserDashboard/pages/MyCampaign'
-
 import HelpCenter from '../features/AdvertiserDashboard/pages/HelpCenter'
 import AddFunds from '../features/AdvertiserDashboard/pages/AddFunds'
-import MyCampaign from './../features/AdvertiserDashboard/pages/MyCampaign';
 import Overview from '../features/AdvertiserDashboard/pages/Overview';
 
 const AdvertiserDashboardRouter = () => {
@@ -35,7 +33,12 @@ const AdvertiserDashboardRouter = () => {
       path: 'overview',
       component: <Overview />,
       exact: true
-    }
+    },
+    {
+      path: 'finance',
+      component: <Overview />,
+      exact: true
+    },
   ]
   return (
 
